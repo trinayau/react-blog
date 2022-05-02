3cb897bf4b5886c51d719daf262153841661d0b1
Trina <92634994+trinayau@users.noreply.github.com>
2022-05-02 20:28:41 +0100

    added styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-
+import './index.css';
+import Navbar from './Navbar'
+import Home from './Home'
 function App() {
   const title = "Welcome to Trina's new blog";
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>{ title }</h1>
+        <Home />
       </div>
     </div>
   );
